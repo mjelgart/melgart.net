95467282adf5619dc550de464623bf98c8aaea7e
Michael Elgart <mje@michaelelgart.com>
2024-05-29 03:08:20 +0000

    ok this time fixed it for real

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../utils/posts';
 import Link from 'next/link';
 import Date from '../components/date';
-import Metadata from '../components/metadata';
+import injectMetadata from '../components/injectMetadata';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -21,7 +21,24 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <Metadata title={siteTitle} description={description} />
+        {injectMetadata(siteTitle, description)}
+        {/* <title>{siteTitle}</title>
+        <meta
+            name="description"
+            content={description}
+        />
+        <meta name="og:title" content={siteTitle} />
+
+        {/* facebook 
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:description" content={description} />
+
+        {/* twitter 
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="melgart.net" />
+        <meta name="twitter:title" content={siteTitle}/>
+        <meta name="twitter:description" content={description} /> */}
       </Head>
       <div className={utilStyles.column}>
       <h1 className={utilStyles.headingXl}>Michael Elgart</h1>
