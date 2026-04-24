e044cef80562711c806eeb06c211feffc7cbc5bc
mjelgart <me@melgart.net>
2026-04-24 12:03:56 +0000

    feat(astro): port layout + static pages

---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+// Placeholder Search component for Commit 3
+// Full implementation will be added in the next task
+
+export default function Search() {
+  return (
+    <div>
+      <p><em>Search functionality will be implemented in the next task.</em></p>
+    </div>
+  );
+}
